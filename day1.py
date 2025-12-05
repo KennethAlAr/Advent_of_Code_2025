d70858af16762b4a13dae1eb23fc2f7313252435
KennethAlAr <kennethalonsoarce@gmail.com>
2025-12-05 17:24:51 +0100

    first commit

---
+++ day1.py
@@ -0,0 +1,36 @@
+'''
+cargar archivo
+[0..99]
+resultado = 0
+para cada lineadel archivo
+    si dial acaba en 0
+    resultado + 1
+
+devolver el resultado
+'''
+
+def part1(input):
+    result = 0
+    dial = 50
+
+    for line in input:
+        rotation = line[0]
+        number = int(line[1:])
+        if rotation == "R":
+            dial = (dial+number)%100
+        else:
+            dial = (dial-number)%100
+        if dial == 0:
+            result += 1
+
+
+    return result
+
+def part2(input):
+    pass
+
+input = list()
+with open("day1.txt", "r") as f:
+    input = f.readlines()
+
+print(f"Part 1: {part1(input)}")
