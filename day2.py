1794a9d3351a68aeb6176830468a1f7506d2346f
KennethAlAr <kennethalonsoarce@gmail.com>
2025-12-05 23:48:00 +0100

    day2 part2

--- day2.py
+++ day2.py
@@ -16,7 +16,29 @@ def part1(input):
 
 
 def part2(input):
-    pass
+    result = 0
+    for group in input:
+        first = int(group.split("-")[0])
+        last = int(group.split("-")[1])
+        for number in range(first, last+1):
+            for i in range(2, (len(str(number))+1)):
+                if len(str(number))%i == 0:
+                    num_list = split_num(number, i)
+                    if len(set(num_list)) == 1:
+                        result += number
+                        break
+    
+    return result
+
+
+def split_num(num, partes):
+    num_list = []
+    length = len(str(num))//partes
+    for i in range(0, len(str(num)), length):
+        num_list.append(int(str(num)[i:i+length]))
+    return num_list
+
+
 
 input = list()
 with open("day2.txt", "r") as f:
@@ -25,4 +47,4 @@ with open("day2.txt", "r") as f:
 
 print(f"Part 1: {part1(input)}")
 
-# print(f"Part 2: {part2(input)}")
+print(f"Part 2: {part2(input)}")
