5aeec99a014fb5cbb73c59aef21f4b066793aa03
KennethAlAr <kennethalonsoarce@gmail.com>
2025-12-06 16:52:09 +0100

    day3 part2

--- day3.py
+++ day3.py
@@ -20,10 +20,27 @@ def part1(input):
 
 
 def part2(input):
-    pass
+    result = 0
+    for line in input:
+        numbers = []
+        for i in range (0,12):
+            for x in range(len(str(line).strip())):
+                candidate = "".join(sorted(line, reverse=True))[x]
+                candidate_pos = line.find(candidate)
+                max_pos = len(str(line.strip()))-12+i
+                if candidate_pos <= max_pos:
+                    numbers.append(str(candidate))
+                    line = line[candidate_pos+1:]
+                    break
+        number = int("".join(numbers))
+        result += number
+    return result
+
 
 input = list()
 with open("day3.txt", "r") as f:
     input = f.readlines()
 
 print(f"Part 1: {part1(input)}")
+
+print(f"Part 2: {part2(input)}")
